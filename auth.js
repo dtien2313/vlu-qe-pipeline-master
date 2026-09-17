f595c1fef8aaa959cd578311c6996321a0b64a8d
dtien2313 <ductien125048@gmail.com>
2026-09-17 19:52:24 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 // Login function
 function login(username, password) {
-    if (username === 'admin' && password === '123') {
+    if (username === 'admin' && password === '') {
         return true;
     }
 
